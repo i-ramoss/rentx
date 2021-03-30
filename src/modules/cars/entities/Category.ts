86412e2973508fa641acf5208ec36dd33109b1ca
Ian Ramos <ianramossantos@hotmail.com>
2021-03-30 10:10:51 -0300

    refactor: create category entity

---
+++ src/modules/cars/entities/Category.ts
@@ -0,0 +1,23 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+@Entity()
+class Category {
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) this.id = uuidV4();
+  }
+}
+
+export { Category };
