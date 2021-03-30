9d4fb732fa8bb51d9a210db779515507001ce10d
Ian Ramos <ianramossantos@hotmail.com>
2021-03-30 09:01:05 -0300

    config: create postgres container and config db connection with app

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,15 @@
-import { createConnection } from 'typeorm';
+import { createConnection, getConnectionOptions } from 'typeorm';
 
-createConnection();
+interface IOptions {
+  host: string;
+}
+
+getConnectionOptions().then(options => {
+  const newOptions = options as IOptions;
+
+  newOptions.host = 'database';
+
+  createConnection({
+    ...options,
+  });
+});
