78ef75de7cc315dd00ed9dda36cfe9ce0808cfca
Ian Ramos <ianramossantos@hotmail.com>
2021-04-01 18:34:43 -0300

    refactor: inject dependencies into createCategory useCase with tsyringe

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -1,15 +1,15 @@
-import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
-import { CreateCategoryController } from './CreateCategoryController';
-import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+// import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
+// import { CreateCategoryController } from './CreateCategoryController';
+// import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
-export default (): CreateCategoryController => {
-  const categoriesRepository = new CategoriesRepository();
+// export default (): CreateCategoryController => {
+//   const categoriesRepository = new CategoriesRepository();
 
-  const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+//   const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
 
-  const createCategoryController = new CreateCategoryController(
-    createCategoryUseCase
-  );
+//   const createCategoryController = new CreateCategoryController(
+//     createCategoryUseCase
+//   );
 
-  return createCategoryController;
-};
+//   return createCategoryController;
+// };
