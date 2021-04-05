d57d5ef2f21ac67dd031d7000de215fdfeb9e114
Ian Ramos <ianramossantos@hotmail.com>
2021-04-05 19:25:15 -0300

    refactor: finish dependency injections with tsyringe in specifications

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,11 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 
-import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
+import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController';
 
 const specificationsRoutes = Router();
 
-specificationsRoutes.post('/', (request: Request, response: Response) => {
-  return createSpecificationController.handle(request, response);
-});
+const createSpecificationController = new CreateSpecificationController();
+
+specificationsRoutes.post('/', createSpecificationController.handle);
 
 export { specificationsRoutes };
