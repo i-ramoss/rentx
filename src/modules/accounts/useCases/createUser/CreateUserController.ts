49b2eaaee121ac564144fb89f20ad68474551887
Ian Ramos <ianramossantos@hotmail.com>
2021-04-06 11:16:57 -0300

    chore: create controller of creation of user

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+class CreateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      const { name, username, email, password, driver_license } = request.body;
+
+      const createUserUseCase = container.resolve(CreateUserUseCase);
+
+      await createUserUseCase.execute({
+        name,
+        username,
+        email,
+        password,
+        driver_license,
+      });
+
+      return response.status(201).send();
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  }
+}
+
+export { CreateUserController };
