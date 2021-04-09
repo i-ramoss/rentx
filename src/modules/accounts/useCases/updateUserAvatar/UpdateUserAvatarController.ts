b7f58a9072e3706a48c1466e63e4e6c885e26ea2
Ian Ramos <ianramossantos@hotmail.com>
2021-04-09 17:30:44 -0300

    feat: configure file and route for uploading user avatar

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -6,7 +6,7 @@ import { UpdateUserAvatarUseCase } from './UpdateUserAvatarUseCase';
 class UpdateUserAvatarController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { id: user_id } = request.user;
-    const avatar_file = null;
+    const avatar_file = request.file.filename;
 
     const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
 
