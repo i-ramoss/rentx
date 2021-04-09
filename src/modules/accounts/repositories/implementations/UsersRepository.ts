5a63d50e55cb57bdb2106e4116855fe552a9253d
Ian Ramos <ianramossantos@hotmail.com>
2021-04-09 11:30:03 -0300

    feat: create user verification method by id

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -32,6 +32,12 @@ class UsersRepository implements IUsersRepository {
 
     return user;
   }
+
+  async findById(id: string): Promise<User> {
+    const user = await this.repository.findOne(id);
+
+    return user;
+  }
 }
 
 export { UsersRepository };
