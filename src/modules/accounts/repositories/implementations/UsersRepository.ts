0f2c4957ce1f43b76c5a5c1fa6e8e13788a31202
Ian Ramos <ianramossantos@hotmail.com>
2021-04-09 17:31:51 -0300

    fix: declare and implement user id and avatar

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -12,16 +12,20 @@ class UsersRepository implements IUsersRepository {
   }
 
   async create({
+    id,
     name,
     password,
     email,
     driver_license,
+    avatar,
   }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
+      id,
       name,
       password,
       email,
       driver_license,
+      avatar,
     });
 
     await this.repository.save(user);
