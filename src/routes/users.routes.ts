f8eb3c97dfbaf110c90fbd4e2402a92d2bce4aba
Ian Ramos <ianramossantos@hotmail.com>
2021-04-09 16:30:56 -0300

    chore: create routes to upload user avatar image

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post('/', createUserController.handle);
+usersRoutes.patch('/avatar', updateUserAvatarController.handle);
 
 export { usersRoutes };
