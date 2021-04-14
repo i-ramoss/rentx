3e876b63c50aefc7aa22db8ccd9b12e8f9f6bb4d
Ian Ramos <ianramossantos@hotmail.com>
2021-04-14 12:04:36 -0300

    refactor: update imports using typescript paths

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -2,7 +2,7 @@ import csvParse from 'csv-parse';
 import fs from 'fs';
 import { inject, injectable } from 'tsyringe';
 
-import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 
 interface IImportCategory {
   name: string;
--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,10 @@ import swaggerUi from 'swagger-ui-express';
 
 import './database';
 
-import './shared/container';
+import '@shared/container';
 
-import { AppError } from './errors/AppError';
+import { AppError } from '@errors/AppError';
+
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 
