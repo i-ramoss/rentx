598d0fe626554eb7c54a53fc5a11ace55d99aa76
Ian Ramos <ianramossantos@hotmail.com>
2021-04-15 18:25:09 -0300

    chore: create cars repository

---
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -0,0 +1,33 @@
+import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO';
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+
+class CarsRepositoryInMemory implements ICarsRepository {
+  cars: Car[] = [];
+
+  async create({
+    name,
+    description,
+    daily_rate,
+    license_plate,
+    fine_amount,
+    brand,
+    category_id,
+  }: ICreateCarDTO): Promise<void> {
+    const car = new Car();
+
+    Object.assign(car, {
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    });
+
+    this.cars.push(car);
+  }
+}
+
+export { CarsRepositoryInMemory };
