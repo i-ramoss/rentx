7a7d353c9f7f80ef40825a41ca27e1e725e97fb4
Ian Ramos <ianramossantos@hotmail.com>
2021-04-15 10:28:35 -0300

    refactor: change import of user and cars repositories

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,5 +1,6 @@
+import { User } from '@modules/accounts/infra/typeorm/entities/User';
+
 import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
-import { User } from '../entities/User';
 
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
