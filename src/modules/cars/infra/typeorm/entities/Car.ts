66a3820e235c0ce6bc7f7df7486fdd74ab7d8031
Ian Ramos <ianramossantos@hotmail.com>
2021-04-15 18:23:30 -0300

    chore: add car entity

---
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -0,0 +1,23 @@
+class Car {
+  id: string;
+
+  name: string;
+
+  description: string;
+
+  daily_rate: number;
+
+  available: boolean;
+
+  license_plate: string;
+
+  fine_amount: number;
+
+  brand: string;
+
+  category_id: string;
+
+  created_at: Date;
+}
+
+export { Car };
