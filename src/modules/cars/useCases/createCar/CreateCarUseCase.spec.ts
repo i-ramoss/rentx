db107c97c75be6ce7deddafe2269a22f7ddd5867
Ian Ramos <ianramossantos@hotmail.com>
2021-04-15 18:26:31 -0300

    jest: start test to create a new car

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,25 @@
+import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+
+import { CreateCarUseCase } from './CreateCarUseCase';
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe('Create Car', () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+  });
+
+  it('should be able to create a new car', async () => {
+    await createCarUseCase.execute({
+      name: 'Car name',
+      description: 'Car description',
+      daily_rate: 250,
+      license_plate: 'ABC-123',
+      fine_amount: 120,
+      brand: 'brand',
+      category_id: 'Category',
+    });
+  });
+});
