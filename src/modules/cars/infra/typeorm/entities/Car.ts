1191eab43b5a3577d24afe51b80c818e5a20ef64
Ian Ramos <ianramossantos@hotmail.com>
2021-08-10 08:44:55 -0300

    chore: update car entity using typeorm

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,32 +1,46 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
+import { Category } from './Category';
+
+@Entity()
 class Car {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   description: string;
 
+  @Column()
   daily_rate: number;
 
-  available: boolean;
+  @Column()
+  available: true;
 
+  @Column()
   license_plate: string;
 
+  @Column()
   fine_amount: number;
 
+  @Column()
   brand: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
-    if (!this.id) {
-      this.id = uuidV4();
-      this.available = true;
-      this.created_at = new Date();
-    }
+    if (!this.id) this.id = uuidV4();
   }
 }
 
