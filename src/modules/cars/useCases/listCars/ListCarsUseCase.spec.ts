4e8940fac5f419a5cd376e43957e2bc8a893bea1
Ian Ramos <ianramossantos@hotmail.com>
2021-08-11 09:46:59 -0300

    test: check the searh for available cars by brand or category

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -27,7 +27,7 @@ describe('Cars listing', () => {
     expect(cars).toEqual([car]);
   });
 
-  it('should be able to list all available cars by car name', async () => {
+  it('should be able to list all available cars by name', async () => {
     const car = await carsRepositoryInMemory.create({
       name: 'Car 02',
       description: 'Car Description',
@@ -38,7 +38,39 @@ describe('Cars listing', () => {
       category_id: 'category_id',
     });
 
-    const cars = await listCarsUseCase.execute({ brand: 'Car Brand' });
+    const cars = await listCarsUseCase.execute({ name: 'Car 02' });
+
+    expect(cars).toEqual([car]);
+  });
+
+  it('should be able to list all available cars by brand', async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: 'Car test',
+      description: 'Car Description',
+      daily_rate: 140,
+      license_plate: 'ABC-1234',
+      fine_amount: 80,
+      brand: 'Car Brand test',
+      category_id: 'category_id',
+    });
+
+    const cars = await listCarsUseCase.execute({ brand: 'Car Brand test' });
+
+    expect(cars).toEqual([car]);
+  });
+
+  it('should be able to list all available cars by category', async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: 'Car test',
+      description: 'Car Description',
+      daily_rate: 140,
+      license_plate: 'ABC-1234',
+      fine_amount: 80,
+      brand: 'Car Brand test',
+      category_id: '99999',
+    });
+
+    const cars = await listCarsUseCase.execute({ category_id: '99999' });
 
     expect(cars).toEqual([car]);
   });
