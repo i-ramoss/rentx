51dc550ca5dd677f6ecc8d44a22dd2289ee90212
Ian Ramos <ianramossantos@hotmail.com>
2021-08-11 08:56:20 -0300

    refactor: list all available cars by name, category or brand

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -35,8 +35,17 @@ class CarsRepositoryInMemory implements ICarsRepository {
     return this.cars.find(car => car.license_plate === license_plate);
   }
 
-  async findAvailable(): Promise<Car[]> {
-    const cars = this.cars.filter(car => car.available === true);
+  async findAvailable(name?: string, brand?: string, category_id?: string): Promise<Car[]> {
+    const cars = this.cars.filter(car => {
+      if (
+        car.available &&
+        ((name && car.name === name) ||
+          (brand && car.brand === brand) ||
+          (category_id && car.category_id === category_id))
+      )
+        return car;
+      return null;
+    });
 
     return cars;
   }
--- src/modules/cars/useCases/listCars/ListCarsUseCase.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.ts
@@ -1,11 +1,16 @@
 import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 
+interface IRequest {
+  name?: string;
+  brand?: string;
+  category_id?: string;
+}
 class ListCarsUseCase {
   constructor(private carsRepository: ICarsRepository) {}
 
-  async execute(): Promise<Car[]> {
-    const cars = this.carsRepository.findAvailable();
+  async execute({ name, brand, category_id }: IRequest): Promise<Car[]> {
+    const cars = this.carsRepository.findAvailable(name, brand, category_id);
 
     return cars;
   }
