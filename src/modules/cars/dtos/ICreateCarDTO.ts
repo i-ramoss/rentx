4fe68c17dfb69d0689e9cf8121ff3e54c2b69f4b
Ian Ramos <ianramossantos@hotmail.com>
2021-08-13 08:08:55 -0300

    chore: add spec attribute to car

--- src/modules/cars/dtos/ICreateCarDTO.ts
+++ src/modules/cars/dtos/ICreateCarDTO.ts
@@ -1,3 +1,5 @@
+import { Specification } from '@modules/cars/infra/typeorm/entities/Specification';
+
 interface ICreateCarDTO {
   name: string;
   description: string;
@@ -6,6 +8,7 @@ interface ICreateCarDTO {
   fine_amount: number;
   brand: string;
   category_id: string;
+  specifications?: Specification[];
 }
 
 export { ICreateCarDTO };
