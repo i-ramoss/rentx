4316f4dab432984539faebd804a3b471f0a84f2f
Ian Ramos <ianramossantos@hotmail.com>
2021-08-13 08:14:13 -0300

    feat: create use case to create an car specification

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -1,4 +1,5 @@
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 
 import { AppError } from '../../../../shared/errors/AppError';
 
@@ -8,12 +9,21 @@ interface IRequest {
 }
 
 class CreateCarSpecificationUseCase {
-  constructor(private carsRepository: ICarsRepository) {}
+  constructor(
+    private carsRepository: ICarsRepository,
+    private specificationsRepository: ISpecificationsRepository
+  ) {}
 
   async execute({ car_id, specifications_id }: IRequest): Promise<void> {
-    const carAlreadyExists = await this.carsRepository.findById(car_id);
+    const carExists = await this.carsRepository.findById(car_id);
 
-    if (!carAlreadyExists) throw new AppError('Car doest not exists!');
+    if (!carExists) throw new AppError('Car doest not exists!');
+
+    const specifications = await this.specificationsRepository.findByIds(specifications_id);
+
+    carExists.specifications = specifications;
+
+    await this.carsRepository.create(carExists);
   }
 }
 
