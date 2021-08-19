7224b6ed88baec137cc3be98b5408ce098b92cd6
Ian Ramos <ianramossantos@hotmail.com>
2021-08-19 10:27:42 -0300

    feat: finish a use case to create a new rental

--- src/modules/rentals/useCases/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/CreateRentalUseCase.ts
@@ -1,3 +1,4 @@
+import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { AppError } from '@shared/errors/AppError';
 
 import { IRentalsRepository } from '../repositories/IRentalsRepository';
@@ -11,12 +12,16 @@ interface IRequest {
 class CreateRentalUseCase {
   constructor(private rentalsRepository: IRentalsRepository) {}
 
-  async execute({ user_id, car_id, expected_return_date }: IRequest): Promise<void> {
+  async execute({ user_id, car_id, expected_return_date }: IRequest): Promise<Rental> {
     const rentalOpenToCar = await this.rentalsRepository.findOpenRentalByCar(car_id);
     const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(user_id);
 
     if (rentalOpenToCar) throw new AppError('There is a rental in progress for car!');
     if (rentalOpenToUser) throw new AppError('There is a rental in progress for user!');
+
+    const rental = await this.rentalsRepository.create({ user_id, car_id, expected_return_date });
+
+    return rental;
   }
 }
 
