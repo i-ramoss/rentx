9714a3d61c8654496af6f23f9565544d5c0f2816
Ian Ramos <ianramossantos@hotmail.com>
2021-08-19 10:25:10 -0300

    chore: create the method of creating a rental

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -1,3 +1,4 @@
+import { ICreateRentalDTO } from '@modules/rentals/dtos/ICreateRentalDTO';
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 
 import { IRentalsRepository } from '../IRentalsRepository';
@@ -6,11 +7,21 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
   rentals: Rental[] = [];
 
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    return this.rentals.find(rental => rental.car_id === car_id && rental.end_date === null);
+    return this.rentals.find(rental => rental.car_id === car_id && !rental.end_date);
   }
 
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    return this.rentals.find(rental => rental.user_id === user_id && rental.end_date === null);
+    return this.rentals.find(rental => rental.user_id === user_id && !rental.end_date);
+  }
+
+  async create({ user_id, car_id, expected_return_date }: ICreateRentalDTO): Promise<Rental> {
+    const rental = new Rental();
+
+    Object.assign(rental, { user_id, car_id, expected_return_date, start_date: new Date() });
+
+    this.rentals.push(rental);
+
+    return rental;
   }
 }
 
