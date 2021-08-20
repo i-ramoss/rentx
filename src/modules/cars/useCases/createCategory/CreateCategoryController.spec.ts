0e38bbdce408cbda684da7410a91f577a14af38f
Ian Ramos <ianramossantos@hotmail.com>
2021-08-20 14:08:13 -0300

    test: ensure that two cateogories with the same name cannot be created

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -45,4 +45,20 @@ describe('Create Category Controller', () => {
 
     expect(response.status).toBe(201);
   });
+
+  it('should not be able to create a new category  with existing name', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@rentx.com.br',
+      password: 'admin',
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post('/categories')
+      .send({ name: 'Category Supertest', description: 'Category Supertest description' })
+      .set({ Authorization: `Bearer ${token}` });
+
+    expect(response.status).toBe(400);
+  });
 });
