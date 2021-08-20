54ecf2eda9f8577056d2a73fa6e6d095e93ac8c8
Ian Ramos <ianramossantos@hotmail.com>
2021-08-20 12:10:17 -0300

    chore: inject rent repository and data provider on rental creation use case

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,3 +1,5 @@
+import { inject, injectable } from 'tsyringe';
+
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
 import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
@@ -9,8 +11,14 @@ interface IRequest {
   expected_return_date: Date;
 }
 
+@injectable()
 class CreateRentalUseCase {
-  constructor(private rentalsRepository: IRentalsRepository, private dateProvider: IDateProvider) {}
+  constructor(
+    @inject('RentalsRepository')
+    private rentalsRepository: IRentalsRepository,
+    @inject('DayJsDateProvider')
+    private dateProvider: IDateProvider
+  ) {}
 
   async execute({ user_id, car_id, expected_return_date }: IRequest): Promise<Rental> {
     const minimumRentalHour = 24;
