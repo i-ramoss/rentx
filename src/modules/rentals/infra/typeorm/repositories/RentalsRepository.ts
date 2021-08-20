61026874eb1ec2bce2db901508d0837153f28f7a
Ian Ramos <ianramossantos@hotmail.com>
2021-08-20 12:11:43 -0300

    chore: create rentals repository and add typeorm on rental entity

---
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -0,0 +1,30 @@
+import { Repository, getRepository } from 'typeorm';
+
+import { ICreateRentalDTO } from '@modules/rentals/dtos/ICreateRentalDTO';
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+
+import { Rental } from '../entities/Rental';
+
+class RentalsRepository implements IRentalsRepository {
+  private repository: Repository<Rental>;
+
+  constructor() {
+    this.repository = getRepository(Rental);
+  }
+
+  async findOpenRentalByCar(car_id: string): Promise<Rental> {
+    return this.repository.findOne({ car_id });
+  }
+  async findOpenRentalByUser(user_id: string): Promise<Rental> {
+    return this.repository.findOne({ user_id });
+  }
+  async create({ user_id, car_id, expected_return_date }: ICreateRentalDTO): Promise<Rental> {
+    const rental = this.repository.create({ user_id, car_id, expected_return_date });
+
+    await this.repository.save(rental);
+
+    return rental;
+  }
+}
+
+export { RentalsRepository };
