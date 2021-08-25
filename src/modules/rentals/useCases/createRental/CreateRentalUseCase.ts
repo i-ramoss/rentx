0f0de142bcd9e3a7f57018ee04092715df38fccd
Ian Ramos <ianramossantos@hotmail.com>
2021-08-25 09:05:06 -0300

    fix: change car availability to false when rented

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
 import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
@@ -17,7 +18,9 @@ class CreateRentalUseCase {
     @inject('RentalsRepository')
     private rentalsRepository: IRentalsRepository,
     @inject('DayJsDateProvider')
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+    @inject('CarsRepository')
+    private carsRepository: ICarsRepository
   ) {}
 
   async execute({ user_id, car_id, expected_return_date }: IRequest): Promise<Rental> {
@@ -35,6 +38,8 @@ class CreateRentalUseCase {
     if (compare < minimumRentalHour) throw new AppError('Invalid return time!');
 
     const rental = await this.rentalsRepository.create({ user_id, car_id, expected_return_date });
+
+    await this.carsRepository.updateAvailable(car_id, false);
 
     return rental;
   }
