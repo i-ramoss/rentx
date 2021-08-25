a9b2514c2e3d088ed96ec5eca3eaea9d91ee421b
Ian Ramos <ianramossantos@hotmail.com>
2021-08-25 10:51:08 -0300

    chore: declare and implement method to fetcha a rent by id

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -15,15 +15,21 @@ class RentalsRepository implements IRentalsRepository {
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
     return this.repository.findOne({ car_id });
   }
+
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
     return this.repository.findOne({ user_id });
   }
+
   async create({ user_id, car_id, expected_return_date }: ICreateRentalDTO): Promise<Rental> {
     const rental = this.repository.create({ user_id, car_id, expected_return_date });
 
     await this.repository.save(rental);
 
     return rental;
+  }
+
+  async findById(rental_id: string): Promise<Rental> {
+    return this.repository.findOne(rental_id);
   }
 }
 
--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -5,6 +5,7 @@ interface IRentalsRepository {
   findOpenRentalByCar(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
   create(data: ICreateRentalDTO): Promise<Rental>;
+  findById(rental_id: string): Promise<Rental>;
 }
 
 export { IRentalsRepository };
