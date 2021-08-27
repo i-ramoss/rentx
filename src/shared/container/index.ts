e24099222172424db30d59ed4544e2e19f17d863
Ian Ramos <ianramossantos@hotmail.com>
2021-08-27 11:55:46 -0300

    chore: instantiate user token repository on tsyringe container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,7 +3,9 @@ import { container } from 'tsyringe';
 import '@shared/container/providers';
 
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
 import { CarsImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarsImagesRepository';
 import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
@@ -29,3 +31,5 @@ container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
 container.registerSingleton<ICarsImagesRepository>('CarsImagesRepository', CarsImagesRepository);
 
 container.registerSingleton<IRentalsRepository>('RentalsRepository', RentalsRepository);
+
+container.registerSingleton<IUsersTokensRepository>('UsersTokensRepository', UsersTokensRepository);
