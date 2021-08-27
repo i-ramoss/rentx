402e0d554f16ed6c1bd9bc3e4ba46bb2fcea1d8c
Ian Ramos <ianramossantos@hotmail.com>
2021-08-27 11:43:53 -0300

    chore: createu user token entity

---
+++ src/modules/accounts/infra/typeorm/entities/UserTokens.ts
@@ -0,0 +1,32 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+import { User } from './User';
+
+@Entity('users_tokens')
+class UserTokens {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  refresh_token: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column()
+  user_id: string;
+
+  @Column()
+  expires_date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) this.id = uuidV4();
+  }
+}
+
+export { UserTokens };
