d77bb718143e82ad379db83db2976ba549ddbb6a
Ian Ramos <ianramossantos@hotmail.com>
2021-08-27 12:29:48 -0300

    feat: create method to add days from current date

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -3,6 +3,7 @@ interface IDateProvider {
   convertToUTC(date: Date): string;
   dateNow(): Date;
   compareInDays(start_date: Date, end_date: Date): number;
+  addDays(days: number): Date;
 }
 
 export { IDateProvider };
