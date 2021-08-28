5de6a1dd4078890056a9391e95a3268ff0bd5374
Ian Ramos <ianramossantos@hotmail.com>
2021-08-28 10:37:06 -0300

    feat: create and implement mail provider to send emails for users

---
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -0,0 +1,43 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import { injectable } from 'tsyringe';
+
+import { IMailProvider } from '../IMailProvider';
+
+@injectable()
+class EtherealMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor() {
+    nodemailer
+      .createTestAccount()
+      .then(account => {
+        const transporter = nodemailer.createTransport({
+          host: account.smtp.host,
+          port: account.smtp.port,
+          secure: account.smtp.secure,
+          auth: {
+            user: account.user,
+            pass: account.pass,
+          },
+        });
+
+        this.client = transporter;
+      })
+      .catch(err => console.error(err));
+  }
+
+  async sendMail(to: string, subject: string, body: string): Promise<void> {
+    const message = await this.client.sendMail({
+      to,
+      from: 'Rentx <noreply@rentx.com.br>',
+      subject,
+      text: body,
+      html: body,
+    });
+
+    console.log('Message sent: %s', message.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
+
+export { EtherealMailProvider };
