3c90b9635f71782bc8a386a85e4a4bfd70c7262d
Ian Ramos <ianramossantos@hotmail.com>
2021-08-28 12:31:23 -0300

    refactor: change the method to send email to receive the email template and its variables

--- src/shared/container/providers/MailProvider/IMailProvider.ts
+++ src/shared/container/providers/MailProvider/IMailProvider.ts
@@ -1,5 +1,5 @@
 interface IMailProvider {
-  sendMail(to: string, subject: string, body: string): Promise<void>;
+  sendMail(to: string, subject: string, variables: any, path: string): Promise<void>;
 }
 
 export { IMailProvider };
--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import handlebars from 'handlebars';
 import nodemailer, { Transporter } from 'nodemailer';
 import { injectable } from 'tsyringe';
 
@@ -26,13 +28,18 @@ class EtherealMailProvider implements IMailProvider {
       .catch(err => console.error(err));
   }
 
-  async sendMail(to: string, subject: string, body: string): Promise<void> {
+  async sendMail(to: string, subject: string, variables: any, path: string): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString('utf-8');
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
     const message = await this.client.sendMail({
       to,
       from: 'Rentx <noreply@rentx.com.br>',
       subject,
-      text: body,
-      html: body,
+      html: templateHTML,
     });
 
     console.log('Message sent: %s', message.messageId);
