57b27cf2f69f748f01a2a035d5cf8b6df013edb6
Ian Ramos <ianramossantos@hotmail.com>
2021-08-30 08:27:48 -0300

    feat: craete method to check if one date is before another

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -5,6 +5,7 @@ interface IDateProvider {
   compareInDays(start_date: Date, end_date: Date): number;
   addDays(days: number): Date;
   addHours(hours: number): Date;
+  compareIfBefore(start_date: Date, end_date: Date): boolean;
 }
 
 export { IDateProvider };
--- src/shared/container/providers/DateProvider/implementations/DayJsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayJsDateProvider.ts
@@ -35,6 +35,10 @@ class DayJsDateProvider implements IDateProvider {
   addHours(hours: number): Date {
     return dayjs().add(hours, 'hour').toDate();
   }
+
+  compareIfBefore(start_date: Date, end_date: Date): boolean {
+    return dayjs(start_date).isBefore(end_date);
+  }
 }
 
 export { DayJsDateProvider };
