d06bfc175a56c5fd03493e77ab1019f4197575f0
Ian Ramos <ianramossantos@hotmail.com>
2021-09-14 17:50:23 -0300

    test: create integration tests to create a new specification

---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.spec.ts
@@ -0,0 +1,57 @@
+import { hash } from 'bcryptjs';
+import request, { Response } from 'supertest';
+import { Connection } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+let connection: Connection;
+
+let responseAdminUserToken: Response;
+
+describe('Create Specification', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+
+    await connection.runMigrations();
+
+    const id = uuidV4();
+    const password = await hash('admin', 8);
+
+    await connection.query(
+      `
+        INSERT INTO users(id, name, email, password, "isAdmin", created_at, driver_license)
+        VALUES('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'xxxxxx')
+      `
+    );
+
+    responseAdminUserToken = await request(app)
+      .post('/sessions')
+      .send({ email: 'admin@rentx.com.br', password: 'admin' });
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to create a new specification', async () => {
+    const response = await request(app)
+      .post('/specifications')
+      .send({ name: 'Specification Test', description: 'Specification Test description' })
+      .set({ Authorization: `Bearer ${responseAdminUserToken.body.refresh_token}` });
+
+    expect(response.status).toBe(201);
+  });
+
+  it('should not be able to create a new specification with existing name', async () => {
+    const response = await request(app)
+      .post('/specifications')
+      .send({ name: 'Specification Test', description: 'Specification Test description' })
+      .set({ Authorization: `Bearer ${responseAdminUserToken.body.refresh_token}` });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({ message: 'Specification already Exists!' });
+  });
+});
