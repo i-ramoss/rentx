59feb0186443e0a7d192719a021be337367b1ca9
Ian Ramos <ianramossantos@hotmail.com>
2021-09-28 08:27:11 -0300

    test: ensure that an image can be added to a user

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.spec.ts
@@ -0,0 +1,44 @@
+import request, { Response } from 'supertest';
+import { Connection } from 'typeorm';
+
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+let connection: Connection;
+
+let responseUserToken: Response;
+
+const testFile = `${__dirname}/../../../../../assets/profile01.jpg`;
+
+describe('Update user avatar', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+
+    await connection.runMigrations();
+
+    await request(app).post('/users').send({
+      name: 'User test',
+      email: 'user@test.com.br',
+      password: '000',
+      driver_license: 'xxxxxxx',
+    });
+
+    responseUserToken = await request(app)
+      .post('/sessions')
+      .send({ email: 'user@test.com.br', password: '000' });
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to add a user avatar', async () => {
+    const response = await request(app)
+      .patch('/users/avatar')
+      .set({ Authorization: `Bearer ${responseUserToken.body.refresh_token}` })
+      .attach('avatar', testFile);
+
+    expect(response.status).toBe(204);
+  });
+});
