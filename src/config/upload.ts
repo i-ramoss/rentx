45fc8555365ece68761467c54b0eeacac91b346e
Ian Ramos <ianramossantos@hotmail.com>
2021-10-06 11:11:28 -0300

    refactor: change the multer's upload directory
    - The multer will no longer be responsible for uploading the files in their appropriate folders.
    - The multer will read the file so that we can upload it, regardless of whether it will be to local storage or S3 storage.

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,18 +2,18 @@ import crypto from 'crypto';
 import multer from 'multer';
 import { resolve } from 'path';
 
-export default {
-  upload(folder: string): multer.Options {
-    return {
-      storage: multer.diskStorage({
-        destination: resolve(__dirname, '..', '..', folder),
-        filename: (request, file, callback) => {
-          const fileHash = crypto.randomBytes(16).toString('hex');
-          const fileName = `${fileHash}-${file.originalname}`;
+const tmpFolder = resolve(__dirname, '..', '..', 'tmp');
 
-          return callback(null, fileName);
-        },
-      }),
-    };
-  },
+export default {
+  tmpFolder,
+
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename: (request, file, callback) => {
+      const fileHash = crypto.randomBytes(16).toString('hex');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
 };
