74720c08dba8c8b34f5c55f788342dce81b0eaae
Ian Ramos <ianramossantos@hotmail.com>
2021-10-06 11:21:08 -0300

    chore: instantiate the local storage container in tsyringe

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,7 +4,11 @@ import { IDateProvider } from './DateProvider/IDateProvider';
 import { DayJsDateProvider } from './DateProvider/implementations/DayJsDateProvider';
 import { IMailProvider } from './MailProvider/IMailProvider';
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
+import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
+import { IStorageProvider } from './StorageProvider/IStorageProvider';
 
 container.registerSingleton<IDateProvider>('DayJsDateProvider', DayJsDateProvider);
 
 container.registerInstance<IMailProvider>('EtherealMailProvider', new EtherealMailProvider());
+
+container.registerSingleton<IStorageProvider>('StorageProvider', LocalStorageProvider);
