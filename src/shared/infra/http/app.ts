c94072ce4ceb0fac58a825b683370adae12058fb
Ian Ramos <ianramossantos@hotmail.com>
2021-10-07 11:48:23 -0300

    chore: tell the app where the static image files are

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,6 +6,7 @@ import swaggerUi from 'swagger-ui-express';
 
 import '@shared/container';
 
+import upload from '@config/upload';
 import { AppError } from '@shared/errors/AppError';
 import createConnection from '@shared/infra/typeorm';
 
@@ -19,6 +20,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
+app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
