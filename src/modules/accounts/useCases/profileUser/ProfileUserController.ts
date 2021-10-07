0a233ca82ee660bd1b85ca8a73e6b45a7f4a95e9
Ian Ramos <ianramossantos@hotmail.com>
2021-10-07 11:47:08 -0300

    chore: create user profile controller and route

---
+++ src/modules/accounts/useCases/profileUser/ProfileUserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { ProfileUserUseCase } from './ProfileUserUseCase';
+
+class ProfileUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.user;
+
+    const profileUserUseCase = container.resolve(ProfileUserUseCase);
+
+    const user = await profileUserUseCase.execute(id);
+
+    return response.status(200).json(user);
+  }
+}
+
+export { ProfileUserController };
