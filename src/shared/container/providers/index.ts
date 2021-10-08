2e2303b8193b30a1e69abd00ef840f10e9b85aaa
Ian Ramos <ianramossantos@hotmail.com>
2021-10-07 21:12:47 -0300

    refactor: abstraction of injection from providers
    each provider will be responsible for making its own injections

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,17 +1,3 @@
-import { container } from 'tsyringe';
-
-import { IDateProvider } from './DateProvider/IDateProvider';
-import { DayJsDateProvider } from './DateProvider/implementations/DayJsDateProvider';
-import { IMailProvider } from './MailProvider/IMailProvider';
-import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
-import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
-import { S3StorageProvider } from './StorageProvider/implementations/S3StorageProvider';
-import { IStorageProvider } from './StorageProvider/IStorageProvider';
-
-container.registerSingleton<IDateProvider>('DayJsDateProvider', DayJsDateProvider);
-
-container.registerInstance<IMailProvider>('EtherealMailProvider', new EtherealMailProvider());
-
-const diskStorage = { local: LocalStorageProvider, s3: S3StorageProvider };
-
-container.registerSingleton<IStorageProvider>('StorageProvider', diskStorage[process.env.disk]);
+import './DateProvider';
+import './MailProvider';
+import './StorageProvider';
