e74d1e700cea229725681d6ae24603fcc6c38f6f
Ian Ramos <ianramossantos@hotmail.com>
2021-10-09 20:11:19 -0300

    chore: add plugin to babel config to stop warning

--- babel.config.js
+++ babel.config.js
@@ -19,5 +19,6 @@ module.exports = {
     'babel-plugin-transform-typescript-metadata',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
   ],
 };
