14c70f6bb8edace334c560ee8472844b2c475188
Ian Ramos <ianramossantos@hotmail.com>
2021-10-10 12:47:58 -0300

    chore: add cors on the app
    allow other applications to use ours

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import 'dotenv/config';
+import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
@@ -23,6 +24,9 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
+
+// como a nossa aplicação estará aberta para todos, não iremos definir nenhuma regra ou permissão
+app.use(cors());
 
 app.use(router);
 
