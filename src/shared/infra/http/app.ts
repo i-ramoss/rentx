cc775a7047ca70e44554252215324e8c220b910c
Ian Ramos <ianramossantos@hotmail.com>
2021-10-27 11:25:52 -0300

    fix: remove sentry initialization when testing

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -22,14 +22,16 @@ const app = express();
 
 app.use(rateLimiter);
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  integrations: [
-    new Sentry.Integrations.Http({ tracing: true }),
-    new Tracing.Integrations.Express({ app }),
-  ],
-  tracesSampleRate: 1.0,
-});
+if (process.env.NODE_ENV !== 'test') {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+}
 
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
